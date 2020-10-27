5d4da6f79b5fcfcf4c279d5c052344d79fb2df71
gcarna <giuseppe.online92@gmail.com>
2020-10-27 01:50:32 +0100

    Initial Commit

---
+++ Luna/LunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaApp.swift
+//  Luna
+//
+//  Created by Giuseppe Carnà on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
